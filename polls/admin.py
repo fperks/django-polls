eefcedfc5c48bab95caddf2f61ac429bf5e32dd4
Frank <frank.perks@gmail.com>
2014-10-08 14:28:32 -0400

    Made Poll Modifiable by Admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from polls.models import Choice, Question
 
-# Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': ['question_text']
+        }),
+        ('Date Information', {
+            'fields' : ['publish_date'],
+            'classes': ['collapse']
+        })
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
+
+
